b059398ec12633ed664348537be82facc41f2396
Jason LeGlue <jasonleglue@gmail.com>
2026-04-13 18:45:48 -0500

    Comitting changes to jl branch before pulling develop

--- scripts/database.js
+++ scripts/database.js
@@ -16,20 +16,26 @@ const database = {
     { id: 6, serviceId: [], name: "Pine Bluff Trails", location: "southeast" },
   ],
   services: [
-    {id: 1, name: "rafting"},
-    {id: 2, name: "canoeing"},
-    {id: 3, name: "fishing"},
-    {id: 4, name: "hiking"},
-    {id: 5, name: "picnicking"},
-    {id: 6, name: "rock climbing"},
-    {id: 7, name: "lodging"},
-    {id: 8, name: "parking"},
-    {id: 9, name: "information"},
-    {id: 10, name: "zip lines"},
-  ]
+    { id: 1, name: "rafting" },
+    { id: 2, name: "canoeing" },
+    { id: 3, name: "fishing" },
+    { id: 4, name: "hiking" },
+    { id: 5, name: "picnicking" },
+    { id: 6, name: "rock climbing" },
+    { id: 7, name: "lodging" },
+    { id: 8, name: "parking" },
+    { id: 9, name: "information" },
+    { id: 10, name: "zip lines" },
+  ],
 };
 
 //functions to getAreas, getServices, getGuests - return structured clone
+export const getGuests = () => {
+  return structuredClone(database.guests);
+};
+export const getLocations = () => {
+  return structuredClone(database.locations);
+};
 export const getServices = () => {
-    return structuredClone(database.services);
-}
+  return structuredClone(database.services);
+};
--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -1,5 +1,20 @@
+import { getLocations } from "./database.js";
+import { getServices } from "./database.js";
 //import getAreas function from database
 
 //function that creates areasHTML that we can display on webpage
+export const renderLocations = () => {
+  let locationsHTML = "";
+  let servicesString = "";
+  let locations = getLocations();
+  let services = getServices();
 
+  for (const location of locations) {
+    locationsHTML += `<div class="locationCard"><h3>${location.name}</h3><p>Services: ${servicesString}</p></div>`;
+  }
+  return locationsHTML;
+
+  // services a location provides is found in locations.serviceId array.
+  //for item of serviceId array, if item === services.id, servicesString += `services[${services.id}-1].name`
+};
 //event listener/function that says displays how many guests are in area when area is clicked
