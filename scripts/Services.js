ce16f52ef9ccf26db25b55c17f1b436b05c85cd0
Stephanie Mahnke <steph.mahnke@gmail.com>
2026-04-13 18:04:27 -0500

    Added services html

--- scripts/Services.js
+++ scripts/Services.js
@@ -1,4 +1,32 @@
 //import get services function from database
+import { getServices, getLocations } from "./database.js"
+
+const services = getServices()
+const locations = getLocations()
+//Event listener/function to match service to the parks
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        const servicesId = clickTarget.dataset.id
+
+        for (const service of services) {
+            if (service.id === parseInt(serviceId)) {
+                window.alert(`${servicesId} is provided in the folllowing parks: ${location.name}`)
+            }
+        }
+    }
+)
 
 //function to create servicesHTML to display on webpage
-//Event listener/function to match service to the parks
+export const listedServices = () => {
+    let servicesHTML = "<ul>"
+
+    for (const service of services) {
+        servicesHTML += `<li data-id="${services.id}" data-name="${services.name}">${service.name}</li>`
+    }
+
+    servicesHTML += "</ul>"
+
+    return servicesHTML
+}
